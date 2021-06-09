d45234a2b6eb51dc615f51eb2b46081372146c50
yangwei0401 <himoo@Live.com>
2021-06-09 22:44:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MQTT-Client-Framework",
-    platforms: [
-        .iOS(.v8)
-
-    ],
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
